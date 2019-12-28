3c9d4019d4848bd07a93e2e0e0567f6fa9304788
insafmechergui <insaf_mechergui@outlook.com>
2019-12-28 17:42:26 +0100

    app2

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const port = 3000;
+app.use(express.static('client'));
+app.use(bodyParser());
+
+app.get('/', (req, res) => {
+  // JUST TRY!!
+  res.render('client/index');
+
+});
+
+app.post('/', (req, res, next) => {
+  var request = JSON.stringify(req.body.result);
+var parseRequest = JSON.parse(request);
+console.table(parseRequest)
 
 
+res.end(parseRequest);
+
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
