f352ef991d0745c98bd95e5c530f950220f1b6b2
insafmechergui <insaf_mechergui@outlook.com>
2019-12-30 20:10:54 +0100

    challenge3

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+const bodyParser = require('body-parser')
+
 const port = 5000;
 
-
+// app.get('/accout', (req, res) => {
+//     res.render('index')
+// })
 app.post('/account', (req, res, next) => {
     console.log(req.body)
 })
